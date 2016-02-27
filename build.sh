f0f8367920f011a866369ac6896746927d028861
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-02-27 16:39:36 -0500

    inital report

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 LUKS=$(${HOME}/projects/password-store/src/password-store.sh show --phonetic systems/outstandingsound/luks) &&
-    sed -e "s#LUKS_VALUE#${LUKS}#" -e "wpassword-store.tex" passwords.tex &&
+    sed \
+	-e "s#HASH_VALUE#$(git -C ~/.password-store/ rev-parse HEAD)#" \
+	-e "s#LUKS_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic systems/outstandingsound/luks)#" \
+	-e "s#PASSWORD_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic systems/outstandingsound/users/HITXNBTZDSYEJULQ)#" \
+	-e "s#GPG_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic gpg/2016/2018)#" \
+	-e "s#GMAIL_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic public/google/passphrase)#" \
+	-e "wpassword-store.tex" passwords.tex &&
     pdflatex password-store.tex &&
     true
