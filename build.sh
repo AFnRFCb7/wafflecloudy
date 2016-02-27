b04067fe7a05b9a1ffb484b330e2a9283574a564
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-02-27 16:52:01 -0500

    Add a report table.

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-LUKS=$(${HOME}/projects/password-store/src/password-store.sh show --phonetic systems/outstandingsound/luks) &&
+RID=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1) &&
+    HASH=$(git -C ${HOME}/.password-store/ rev-parse HEAD) &&
     sed \
-	-e "s#HASH_VALUE#$(git -C ~/.password-store/ rev-parse HEAD)#" \
+	-e "s#RID_VALUE#${RID}#" \
+	-e "s#REPORT_VALUE#$(git describe --tags --long)#" \
+	-e "s#HASH_VALUE#${HASH}#" \
 	-e "s#LUKS_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic systems/outstandingsound/luks)#" \
 	-e "s#PASSWORD_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic systems/outstandingsound/users/HITXNBTZDSYEJULQ)#" \
 	-e "s#GPG_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic gpg/2016/2018)#" \
 	-e "s#GMAIL_VALUE#$(${HOME}/projects/password-store/src/password-store.sh show --phonetic public/google/passphrase)#" \
 	-e "wpassword-store.tex" passwords.tex &&
+    echo ${HASH},${RID}, >> reports.csv &&
     pdflatex password-store.tex &&
     true
